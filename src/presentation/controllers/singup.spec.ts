85afe4304e0347acca9b9565eb9d43c1fb70852b
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-06-06 18:32:57 -0300

    refactor: change mocking strategy

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -17,15 +17,6 @@ const makeEmailValidator = (): EmailValidator => {
   return new EmailValidatorStub()
 }
 
-const makeEmailValidatorWithError = (): EmailValidator => {
-  class EmailValidatorStub implements EmailValidator {
-    isValid (email: string): boolean {
-      throw new ServerError()
-    }
-  }
-  return new EmailValidatorStub()
-}
-
 const makeSut = (): SutTypes => {
   const emailValidatorStub = makeEmailValidator()
   const sut = new SignUpController(emailValidatorStub)
@@ -135,8 +126,11 @@ describe('SignUp Controller', () => {
   })
 
   test('Should return 500 if EmailValidator throws', () => {
-    const emailValidatorStub = makeEmailValidatorWithError()
-    const sut = new SignUpController(emailValidatorStub)
+    const { sut, emailValidatorStub } = makeSut()
+
+    vitest
+      .spyOn(emailValidatorStub, 'isValid')
+      .mockImplementationOnce(() => { throw new ServerError() })
 
     const httpRequest = {
       body: {
