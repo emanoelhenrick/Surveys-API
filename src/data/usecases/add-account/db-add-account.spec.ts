95f4f0ce5107d828d0f3c04cfa43da123d74b1bb
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-06-07 17:53:48 -0300

    refactor: move sut creation to a factory helper method

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -2,27 +2,36 @@ import { describe, expect, test, vitest } from 'vitest'
 import { DbAddAccount } from './db-add-account'
 import { type Encrypter } from '../../protocols/Encrypter'
 
+interface SutTypes {
+  encrypterStub: Encrypter
+  sut: DbAddAccount
+}
+
+const makeSut = (): SutTypes => {
+  class EncrypterStub implements Encrypter {
+    async encrypt (password: string): Promise<string> {
+      return await new Promise(resolve => { resolve('hashed_password') })
+    }
+  }
+  const encrypterStub = new EncrypterStub()
+  const sut = new DbAddAccount(encrypterStub)
+  return {
+    encrypterStub,
+    sut
+  }
+}
+
 describe('DbAddAccount Usecase', () => {
   test('Should call encrypter with correct password', async () => {
-    class EncrypterStub implements Encrypter {
-      async encrypt (password: string): Promise<string> {
-        return await new Promise(resolve => { resolve('hashed_password') })
-      }
-    }
-
-    const encrypterStub = new EncrypterStub()
-    const sut = new DbAddAccount(encrypterStub)
+    const { sut, encrypterStub } = makeSut()
 
     const encryptSpy = vitest.spyOn(encrypterStub, 'encrypt')
-
     const accountData = {
       name: 'valid_name',
       email: 'valid_email@mail.com',
       password: 'valid_password'
     }
-
     await sut.add(accountData)
-
     expect(encryptSpy).toHaveBeenCalledWith('valid_password')
   })
 })
