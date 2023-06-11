d853b97ec54b0f7725fe227a14ad665fee198b9d
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-06-11 16:24:42 -0300

    test: ensure BcryptAdapter returns a hash on success

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -10,4 +10,12 @@ describe('Bcrypt Adapter', () => {
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
+
+  test('Should return a hash on success', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const hash = await sut.encrypt('any_value')
+    expect(bcrypt.compareSync('any_value', hash))
+      .toBeTruthy()
+  })
 })
