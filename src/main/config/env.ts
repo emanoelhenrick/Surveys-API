7f71842f34feb7101e470ba66f86df2392d4c93d
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-06-13 13:12:31 -0300

    chore: add dotenv

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,3 +1,5 @@
+import ('dotenv/config')
+
 export default {
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://docker:docker@localhost:27017/?authMechanism=DEFAULT',
   port: process.env.PORT ?? 10000
