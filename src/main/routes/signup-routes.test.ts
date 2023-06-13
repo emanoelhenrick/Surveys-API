7bf58a57c98b86253a7bcb85871e992f62787e5e
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-06-13 11:53:38 -0300

    refactor: connect to mongo db before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,8 +1,27 @@
 import request from 'supertest'
 import app from '../config/app'
-import { describe, test } from 'vitest'
+import { afterAll, beforeAll, describe, test } from 'vitest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { beforeEach } from 'node:test'
 
 describe('SignUp Routes', () => {
+  const MONGO_URL = 'mongodb://docker:docker@localhost:27017/?authMechanism=DEFAULT'
+
+  beforeAll(async () => {
+    await MongoHelper.connect(MONGO_URL, 'dev')
+  })
+
+  afterAll(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.drop()
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
