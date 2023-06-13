4bdac6524f261b3286dc6875869c9839dcc9f15e
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-06-13 12:10:29 -0300

    feat: connect to mongo before listen

---
+++ src/main/config/env.ts
@@ -0,0 +1,4 @@
+export default {
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://docker:docker@localhost:27017/?authMechanism=DEFAULT',
+  port: process.env.PORT ?? 5050
+}
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,12 @@
-import app from './config/app'
 
-app.listen(5050, () => {
-  console.log('Server as running at http://localhost:5050')
-})
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+import env from './config/env'
+
+void MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () => {
+      console.log(`Server as running at http://localhost:${env.port}`)
+    })
+  })
+  .catch(console.error)
