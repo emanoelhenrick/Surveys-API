fe7b27865b220f09e6ee960dd7d6826bc9a2462d
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-06-16 18:46:52 -0300

    test: ensure LoginController returns 500 if Authentication throws

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -88,6 +88,15 @@ describe('Login Controller', () => {
     expect(httpResponse).toEqual(serverError(new Error()))
   })
 
+  test('Should return 500 if Authentication throws', async () => {
+    const { sut, authenticationStub } = makeSut()
+    vitest
+      .spyOn(authenticationStub, 'auth')
+      .mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()) }))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
   test('Should call authentication with correct value', async () => {
     const { sut, authenticationStub } = makeSut()
     const authSpy = vitest.spyOn(authenticationStub, 'auth')
