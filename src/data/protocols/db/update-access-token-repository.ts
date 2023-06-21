997da629383a1b1d7b2edcf2e44afd387ed06357
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-06-21 16:55:08 -0300

    feat: ensure AccountMongoRepository updates the account access token on updateAccessToken success

--- src/data/protocols/db/update-access-token-repository.ts
+++ src/data/protocols/db/update-access-token-repository.ts
@@ -1,3 +1,3 @@
 export interface UpdateAccessTokenRepository {
-  updateAccessToken (id: string, token: string): Promise<string>
+  updateAccessToken (id: string, token: string): Promise<void>
 }
--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,10 +1,15 @@
+import { ObjectId } from 'mongodb'
 import { type AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
 import { type LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
+import { type UpdateAccessTokenRepository } from '../../../../data/protocols/db/update-access-token-repository'
 import { type AccountModel } from '../../../../domain/models/account'
 import { type AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
+export class AccountMongoRepository implements
+  AddAccountRepository,
+  LoadAccountByEmailRepository,
+  UpdateAccessTokenRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
@@ -17,5 +22,13 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
     const account = await accountCollection.findOne({ email })
     if (!account) return null
     return MongoHelper.map<AccountModel>(account)
+  }
+
+  async updateAccessToken (id: string, token: string): Promise<void> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: { accessToken: token } }
+    )
   }
 }
