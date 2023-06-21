8231634b1f2660821ab29f32e6b5eac15ef9fcea
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-06-21 15:02:05 -0300

    feat: ensure BcryptAdapter returns true when compare succeeds

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -51,4 +51,11 @@ describe('Bcrypt Adapter', () => {
     await sut.compare('any_value', 'any_hash')
     expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
   })
+
+  test('Should return true when compare succeeds', async () => {
+    const { sut } = makeSut()
+    const hash = await sut.hash('any_value')
+    const isValid = await sut.compare('any_value', hash)
+    expect(isValid).toBeTruthy()
+  })
 })
