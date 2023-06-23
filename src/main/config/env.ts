12f5e9904e3e9dfd1e639c03867bb0ef6ea34169
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-06-23 15:10:57 -0300

    chore: update dependencies

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,7 @@
 import ('dotenv/config')
 
 export default {
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://docker:docker@mongo:27017/?authMechanism=DEFAULT',
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://docker:docker@localhost:27017/?authMechanism=DEFAULT',
   port: process.env.PORT ?? 10000,
   jwt_secret: process.env.JWT_SECRET ?? 'jl39cjjhkdf3h2'
 }
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     include: ['**\/*.{spec,test}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    exclude: ['**/node_modules/**', '**/dist/**', '**/cypress/**', '**/.{idea,git,cache,output,temp}/**']
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/data/**',
+    ]
   }
 })
