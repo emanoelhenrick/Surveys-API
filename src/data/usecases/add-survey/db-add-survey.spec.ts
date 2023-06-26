33d0f5f6f774732da4ca2a522c5fe68c4e708cc8
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-06-26 17:12:15 -0300

    test: ensure DbAddSurvey throw if AddSurveyRepository throws

--- src/data/usecases/add-survey/db-add-survey.spec.ts
+++ src/data/usecases/add-survey/db-add-survey.spec.ts
@@ -42,4 +42,13 @@ describe('DbAddSurvey Usecase', () => {
     await sut.add(surveyData)
     expect(addSpy).toHaveBeenCalledWith(surveyData)
   })
+
+  test('Should throw if AddSurveyRepository throws', async () => {
+    const { sut, addSurveyRepositoryStub } = makeSut()
+    vi.spyOn(addSurveyRepositoryStub, 'add')
+      .mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()) }))
+    const surveyData = makeFakeSurveyData()
+    const error = sut.add(surveyData)
+    await expect(error).rejects.toThrow()
+  })
 })
