7831777b64d5617e9de90d9ed554f03c823e6e62
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-06-26 18:56:18 -0300

    fix: replace instance of validation error

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -10,7 +10,7 @@ export class AddSurveyController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const error = this.validation.validate(httpRequest.body)
-      if (error instanceof Error) return badRequest(new Error())
+      if (error instanceof Error) return badRequest(error)
       const { question, answers } = httpRequest.body
       await this.addSurvey.add({ question, answers })
       return noContent()
