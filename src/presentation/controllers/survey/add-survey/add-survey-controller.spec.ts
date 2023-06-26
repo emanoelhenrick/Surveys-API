79f30befe0127373229ddcab650567b86b7a661d
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-06-26 15:03:18 -0300

    test: ensure AddSurveyController calls validation with correct values

---
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -0,0 +1,31 @@
+import { describe, expect, test, vi } from 'vitest'
+import { type Validation, type HttpRequest } from './add-survey-controller-protocols'
+import { AddSurveyController } from './add-survey-controller'
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    question: 'any-question',
+    answers: [
+      {
+        image: 'any_image',
+        answer: 'any_answer'
+      }
+    ]
+  }
+})
+
+describe('AddSurvey Controller', () => {
+  test('Should call validation with correct values', async () => {
+    class ValidationStub implements Validation {
+      validate (input: any): Error | null {
+        return null
+      }
+    }
+    const validationStub = new ValidationStub()
+    const validateSpy = vi.spyOn(validationStub, 'validate')
+    const sut = new AddSurveyController(validationStub)
+    const httpRequest = makeFakeRequest()
+    await sut.handle(httpRequest)
+    expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+})
