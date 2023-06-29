633995fe84ce5f88af5e0ce4ccc621dc4451b2e0
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-06-29 15:21:15 -0300

    test: ensure JwtAdapter call verify with correct values

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -6,6 +6,12 @@ const makeSut = (): JwtAdapter => {
   return new JwtAdapter('secret')
 }
 
+vi.spyOn(jwt, 'sign')
+  .mockImplementation(() => 'any_token')
+
+vi.spyOn(jwt, 'verify')
+  .mockImplementation(() => 'any_value')
+
 describe('JWT Adapter', () => {
   describe('sing()', () => {
     test('Should call sign with correct values', async () => {
@@ -18,7 +24,8 @@ describe('JWT Adapter', () => {
     test('Should return a token on sign success', async () => {
       const sut = makeSut()
       const accessToken = await sut.encrypt('any_id')
-      expect(accessToken).toEqual(expect.any(String))
+
+      expect(accessToken).toEqual('any_token')
     })
 
     test('Should throw if sign throws', async () => {
@@ -27,6 +34,15 @@ describe('JWT Adapter', () => {
         .mockImplementationOnce(() => { throw new Error() })
       const promise = sut.encrypt('any_id')
       await expect(promise).rejects.toThrow()
+    })
+  })
+
+  describe('verify()', () => {
+    test('Should call verify with correct values', async () => {
+      const sut = makeSut()
+      const verifySpy = vi.spyOn(jwt, 'verify')
+      await sut.decrypt('any_token')
+      expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
     })
   })
 })
