c93ba80959e7648ef3284ebf90ea521693ece770
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-07-06 14:52:15 -0300

    test: ensure DbLoadSurveys calls LoadSurveysRepository

---
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -0,0 +1,40 @@
+import { describe, expect, test, vi } from 'vitest'
+import { type LoadSurveysRepository } from '../../protocols/db/survey/load-survey-repository'
+import { type SurveyModel } from '../../../domain/models/survey'
+import { DbLoadSurveys } from './db-load-surveys'
+
+const makeFakeSurveys = (): SurveyModel[] => {
+  return [{
+    id: 'any_id',
+    question: 'any-question',
+    answers: [{
+      image: 'any_image',
+      answer: 'any_answer'
+    }],
+    date: new Date()
+  },
+  {
+    id: 'other_id',
+    question: 'other-question',
+    answers: [{
+      image: 'other_image',
+      answer: 'other_answer'
+    }],
+    date: new Date()
+  }]
+}
+
+describe('DbLoadSurveys', () => {
+  test('Should call LoadSurveysRepository', async () => {
+    class LoadSurveysRepositoryStub implements LoadSurveysRepository {
+      async loadAll (): Promise<SurveyModel[]> {
+        return makeFakeSurveys()
+      }
+    }
+    const loadSurveysRepositoryStub = new LoadSurveysRepositoryStub()
+    const loadAllSpy = vi.spyOn(loadSurveysRepositoryStub, 'loadAll')
+    const sut = new DbLoadSurveys(loadSurveysRepositoryStub)
+    await sut.load()
+    expect(loadAllSpy).toHaveBeenCalled()
+  })
+})
